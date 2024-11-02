9fdcfe6650309ee1f58a117e1d7f41be1a29d5ce
peter safwat <petersafwat640@gmail.com>
2024-11-03 00:36:04 +0200

    updated some APIs

--- controllers/ratesController.js
+++ controllers/ratesController.js
@@ -1,7 +1,37 @@
 const handlers = require("./handelerFactory");
 
 exports.getAllRates = handlers.getAll("dba.qru_rates");
-exports.getRates= handlers.getOne("dba.qru_rates");
+exports.getRates = handlers.getOne("dba.qru_rates");
 exports.createRates = handlers.createOne("dba.qru_rates");
+// exports.createRates = catchAsync(async (req, res, next) => {
+//   const {
+//     notes,
+//     restrictions,
+//     surcharges,
+//     locations,
+//     restricted_postal_codes,
+//     destination_locations,
+//     origin_locations,
+//   } = req.body;
+
+//   // Basic validation
+//   // Create a new surcharge type in the database
+//   const [newSurchargeType] = await knex("dba.qru_surcharge_type")
+//     .insert({
+//       ...req.body,
+//       general: JSON.stringify(general),
+//       translation: JSON.stringify(translation),
+//       ltl: JSON.stringify(ltl),
+//     })
+//     .returning("*");
+
+//   res.status(201).json({
+//     status: "success",
+//     data: {
+//       data: newSurchargeType,
+//     },
+//   });
+// });
+
 exports.updateRates = handlers.updateOne("dba.qru_rates");
 exports.deleteRates = handlers.deleteOne("dba.qru_rates");
--- routes/chargesRoutes.js
+++ routes/chargesRoutes.js
@@ -1,17 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const chargesController = require("../controllers/chargesController");
-
-router
-  .route("/")
-  .get(chargesController.getAllCharges)
-  .post(chargesController.createCharge);
-
-router
-  .route("/:id")
-  .get(chargesController.getCharge)
-  .patch(chargesController.updateCharge)
-  .delete(chargesController.deleteCharge);
 router
   .route("/termsAndConditions")
   .get(chargesController.getAllTerms)
@@ -23,4 +12,14 @@ router
   .patch(chargesController.updateTerm)
   .delete(chargesController.deleteTerm);
 
+router
+  .route("/")
+  .get(chargesController.getAllCharges)
+  .post(chargesController.createCharge);
+
+router
+  .route("/:id")
+  .get(chargesController.getCharge)
+  .patch(chargesController.updateCharge)
+  .delete(chargesController.deleteCharge);
 module.exports = router;
